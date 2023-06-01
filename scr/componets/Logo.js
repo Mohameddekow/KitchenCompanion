b10369de7cbd818b03ae7f267e0c3b487e0817c7
Mohamed dekow <mohameddekow77@gmail.com>
2023-06-01 10:06:05 +0300

    app logo

--- scr/componets/Logo.js
+++ scr/componets/Logo.js
@@ -1,14 +1,23 @@
 import { View, Text, StyleSheet } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import IconSearch from "react-native-vector-icons/MaterialIcons";
-
+import COLORS from "../conts/colors";
 const AppLogo = () => {
   return (
-    <View style={style.logo}>
-      <Icon name="silverware-variant" size={30} />
-      <Text style={{ marginStart: 10, fontSize: 27, fontWeight: "bold" }}>
-        Delecious
-      </Text>
+    <View>
+      <View style={style.logo}>
+        <Icon name="silverware-variant" size={30} />
+        <Text style={{ marginStart: 10, fontSize: 27, fontWeight: "bold" }}>
+          Delecious
+        </Text>
+      </View>
+
+      <View style={style.welcomeStyle}>
+        <Text style={{ fontSize: 28, fontWeight: "bold" }}>Welcome to</Text>
+        <Text style={{ fontSize: 33, fontWeight: "bold", color: COLORS.green }}>
+          Kitchen Companion
+        </Text>
+      </View>
     </View>
   );
 };
@@ -30,6 +39,14 @@ const style = StyleSheet.create({
     padding: 10,
     alignItems: "center",
     marginTop: 40,
+  },
+
+  welcomeStyle: {
+    width: "100%",
+    flexDirection: "column",
+    justifyContent: "flex-start",
+    alignItems: "flex-start",
+    padding: 10,
   },
 });
 
